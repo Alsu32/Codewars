975cedd4b900e2e2e953a7bfff455b4d14cab21b
Alsu32 <sai-alsu@mail.ru>
2022-10-02 16:59:19 +0300

    Test3

---
+++ Test3.js
@@ -0,0 +1,33 @@
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  let a = d1,
+      b = a2,
+      c = d2;
+  return a+b+c;
+}
+function Bee(){
+  let a = b1,
+      b = e2;
+  return a+b+b;
+}
+function banana(){
+  let a = b2 + a2,
+      b = n2 + a2,
+      c = b;
+  return a+b+c;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+
